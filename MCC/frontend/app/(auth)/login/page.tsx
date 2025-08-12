607e76a52623cb90e3f1246e8c053411bc3389f9
abdelkderboukert <abdelkaderboukart@gmail.com>
2025-08-12 20:54:27 +0100

    feat: Enhance login and registration pages with improved UI and navigation links

--- MCC/frontend/app/(auth)/login/page.tsx
+++ MCC/frontend/app/(auth)/login/page.tsx
@@ -4,6 +4,8 @@ import axiosInstance from "@/lib/axios";
 import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/store/authStore";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -34,24 +36,53 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="size-full flex items-center justify-center min-h-screen bg-white flex-col">
+      <h2 className="text-black text-2xl font-bold">
+        Sign in to B luck Business <br /> Hub
+      </h2>
+      <p className="text-black m-4">
+        Don&apos;t have an account?{" "}
+        <Link href={"/register"} className="font-bold text-blue-800">
+          Create one
+        </Link>{" "}
+      </p>
+      <form
+        className="bg-white shadow-xl w-3/8 h-max p-8 flex flex-col justify-center items-center rounded-lg"
+        onSubmit={handleSubmit}
+      >
+        <div className="h-8 w-4/5 flex justify-start items-end text-black/50 text-sm mb-1">
+          Email
+        </div>
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
           required
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 text-blue-300 rounded-xl"
         />
+        <div className="h-8 w-4/5 flex justify-start items-end text-black/50 text-sm mb-1">
+          Password
+        </div>
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           required
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 text-blue-300 rounded-xl"
         />
-        <button type="submit">Login</button>
+        <div className="h-8 w-4/5 flex justify-start items-center text-blue-500 mb-5 font-bold">
+          Forgot your password?
+        </div>
+        <motion.button
+          type="submit"
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 bg-blue-400 rounded-xl"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          Login
+        </motion.button>
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
     </div>
--- MCC/frontend/app/(auth)/register/page.tsx
+++ MCC/frontend/app/(auth)/register/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import axiosInstance from "@/lib/axios";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
@@ -33,38 +35,70 @@ export default function RegisterPage() {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="size-full flex items-center justify-center min-h-screen bg-white flex-col">
+      <h2 className="text-black text-2xl font-bold">
+        Sign in to B luck Business <br /> Hub
+      </h2>
+      <p className="text-black m-4">
+        You have an account?{" "}
+        <Link href={"/login"} className="font-bold text-blue-800">
+          Login
+        </Link>{" "}
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-xl w-3/8 h-max p-8 flex flex-col justify-center items-center rounded-lg"
+      >
+        <div className="h-8 w-4/5 flex justify-start items-end text-black/50 text-sm mb-1">
+          Username
+        </div>
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 text-blue-300 rounded-xl"
           required
         />
+        <div className="h-8 w-4/5 flex justify-start items-end text-black/50 text-sm mb-1">
+          Email
+        </div>
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 text-blue-300 rounded-xl"
           required
         />
+        <div className="h-8 w-4/5 flex justify-start items-end text-black/50 text-sm mb-1">
+          Password
+        </div>
         <input
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 text-blue-300 rounded-xl"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           required
         />
+        <div className="h-8 w-4/5 flex justify-start items-end text-black/50 text-sm mb-1">
+          Password
+        </div>
         <input
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 text-blue-300 rounded-xl"
           type="password"
           value={password2}
           onChange={(e) => setPassword2(e.target.value)}
           placeholder="Confirm Password"
           required
         />
-        <button type="submit">Register</button>
+        <motion.button
+          type="submit"
+          className="h-14 w-4/5 mb-4 p-2 border border-gray-300 bg-blue-400 rounded-xl"
+        >
+          Register
+        </motion.button>
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
     </div>
