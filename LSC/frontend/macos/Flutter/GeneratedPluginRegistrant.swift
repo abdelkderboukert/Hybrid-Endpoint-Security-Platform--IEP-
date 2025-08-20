84cd8ea7d08598d4ad0de0ab78db6ba325ca03df
abdelkderboukert <abdelkaderboukart@gmail.com>
2025-08-20 20:47:55 +0100

    add login, logout token refresh and dashboard

--- LSC/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ LSC/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
 import macos_window_utils
+import path_provider_foundation
 import screen_retriever
 import system_theme
 import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
