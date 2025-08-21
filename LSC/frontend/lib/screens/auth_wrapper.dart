402b3ea805c9ea2f64c48880cb93e12c24c72c2c
abdelkderboukert <abdelkaderboukart@gmail.com>
2025-08-21 14:35:24 +0100

    add activation code check and change the ui of login page

---
+++ LSC/frontend/lib/screens/auth_wrapper.dart
@@ -0,0 +1,28 @@
+// screens/auth_wrapper.dart
+
+import 'package:flutter/material.dart';
+import 'package:frontend/services/api_service.dart';
+import './auth/activation_page.dart';
+import './dashboard/dashboard_screen.dart';
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: ApiService().checkLicenseStatus(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.data == true) {
+          return const MyHomePage();
+        } else {
+          return const ActivationPage();
+        }
+      },
+    );
+  }
+}
